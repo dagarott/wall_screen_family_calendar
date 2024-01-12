8865972674e6002bae80392cc2195bef577919cb
dagarott <dagarott@gmail.com>
2024-01-12 20:34:12 +0100

    added code to show info in MAX7219 matrix

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QTextCharFormat, QColor
 from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.uic import loadUi
 import sys
 import signal
 import time
+import requests
+import json
 import random
 import datetime
 from datetime import datetime
@@ -16,14 +20,16 @@ from datetime import datetime
 # from luma.core.render import canvas
 # from luma.core.virtual import viewport
 # from luma.core.legacy import text, show_message, textsize
-# from luma.core.legacy.font import proportional ,CP437_FONT, ATARI_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT, ATARI_FONT
+# from luma.core.legacy.font import proportional, CP437_FONT, ATARI_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT, ATARI_FONT
 # documentation: https://www.eclipse.org/paho/clients/python/docs/
 import paho.mqtt.client as mqtt
 
+# Replace with your OpenWeatherMap API key
+API_KEY = "23b1e28665e2cc447d07a3a268c6cede"
 
 # create matrix device
 # serial = spi(port=0, device=1, gpio=noop())
-# device = max7219(serial, cascaded= 8, block_orientation=-90)
+# device = max7219(serial, cascaded=8, block_orientation=-90)
 # print("Created device")
 
 broker = "maqiatto.com"
@@ -34,6 +40,22 @@ client_id = f'publish-{random.randint(0, 1000)}'
 username = "dagarott@gmail.com"
 password = "Dau8queL"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+class SerialPort(QSerialPort):
+    def __init__(self, port_name, baud_rate=QSerialPort.Baud115200):
+        super().__init__()
+        self.setPortName(port_name)
+        self.setBaudRate(baud_rate)
+        self.open(QSerialPort.ReadWrite)
+
+    def write_data(self, data):
+        self.writeData(data.encode())
+
+    def read_data(self):
+        if self.canReadLine():
+            return self.readLine().data().decode().strip()
+        return None
 
 
 class Window(QWidget):
@@ -57,6 +79,19 @@ class Window(QWidget):
         self.update_calendar_status()
         self.update_events_listview()
 
+        # Crear el QTimer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_systick)
+
+        # Iniciar el QTimer con un intervalo de 1000 ms (1 segundo)
+        self.timer.start(1000)
+        # Iniciar el contador systick
+        self.systick = 0
+
+        self.serial_port = None
+
+        self.find_serial_ports()
+
     def calendarDateChanged(self):
         print("The calendar date was changed.")
         dateSelected = self.calendarWidget.selectedDate().toPyDate()
@@ -67,7 +102,6 @@ class Window(QWidget):
         # show_message(device, self.msg, fill="white", font=proportional(CP437_FONT))
 
     def update_events_listview(self):
-
         # Obtener la fecha actual
         currentMonth = datetime.now().month
         currentYear = datetime.now().year
@@ -83,6 +117,7 @@ class Window(QWidget):
             f"SELECT tipo, fecha, hora, descripcion FROM eventos WHERE fecha LIKE '{fecha_actual}%'")
         eventos = c.fetchall()
         conn.close()
+        self.EventList.clear()
         # Agregar los eventos al QListWidget
         for evento in eventos:
             self.EventList.addItem(
@@ -113,7 +148,7 @@ class Window(QWidget):
     def colour_event_day(self, year, month, day):
         # day = QDate(date.year, date.month, date.day)
         format = QTextCharFormat()
-        format.setBackground(QColor(255, 0, 0))
+        format.setBackground(QColor(100, 127, 100))
         self.calendarWidget.setDateTextFormat(QDate(year, month, day), format)
 
     def save_events(self, msg_eventtype, msg_date, msg_time, msg_description):
@@ -143,23 +178,84 @@ class Window(QWidget):
 
     def shown_on_display_coming_event(self):
         # Obtener la fecha de mañana
-        mañana = datetime.date.today() + datetime.timedelta(days=1)
-        print(mañana)
-
+        tomorrow = QDate.currentDate().addDays(1)
+        tomorrow_str = tomorrow.toString(Qt.ISODate)
         # Obtener eventos programados para mañana desde la base de datos
         conn = sqlite3.connect("eventos.db")
         c = conn.cursor()
-        c.execute(
-            "SELECT tipo, fecha, hora, descripcion FROM eventos WHERE fecha = ?;", (mañana,))
-        eventos = c.fetchall()
+        query = f"SELECT * FROM eventos WHERE fecha = '{tomorrow_str}'"
+        c.execute(query)
+        # Recorrer el resultado de la consulta y mostrar los eventos
+        self.temp = c.fetchall()
+        self.length = len(self.temp)
+        print(self.length)
+        print(self.temp)
+        if len(self.temp):
+            self.data = "!!!MANYANA,"
+            # for event in c.fetchall():
+            for idx in range(self.length):
+                self.data += self.temp[idx][0] + ' ' + self.temp[idx][3] + ' ' + self.temp[idx][2] + ' | '
+            print(self.data)
+            self.send_data(self.data)
+
         conn.close()
 
-        if eventos:
-            texto_eventos = "\n".join(
-                [f"{evento[0]} {evento[1]} {evento[2]} {evento[3]}" for evento in eventos])
-            print(texto_eventos)
+    def show_next_month(self):
+        self.calendarWidget.showNextMonth()
+
+    def show_previous_month(self):
+        self.calendarWidget.showPreviousMonth()
+
+    def fetch_weather(self):
+        city = "Alboraya"
+        if not city:
+            return
+
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric&lang=es"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            weather_data = response.json()
+            self.display_weather(weather_data)
         else:
-            print("No hay eventos programados para mañana.")
+            print("Error fetching weather data. Please check the city name.")
+
+    def display_weather(self, weather_data):
+        if "list" not in weather_data:
+            print("No weather data available.")
+            return
+        self.today_weather_data = weather_data['list'][00]
+        self.date = self.today_weather_data["dt_txt"].split()[0]
+        self.current_temperature = self.today_weather_data['main']['temp']
+        self.humidity = self.today_weather_data['main']['humidity']
+        self.tempmin = self.today_weather_data['main']['temp_min']
+        self.tempmax = self.today_weather_data['main']['temp_max']
+        self.descripcion = self.today_weather_data['main']['description']
+        print(f"{weather_data['city']['name']}" + ',' + f"{self.date}" + ',' +
+              f"Temperatura:{self.current_temperature}ºC" + ',' +
+              f"Humedad:{self.humidity}%" + ',' +
+              f"Temp_min:{self.tempmin}ºC" + ','+f"Temp_max:{self.tempmax}ºC")
+        self.data = weather_data['city']['name'] + ',' + "Temp:" + str(self.current_temperature) + "ºC" + ',' + "Hum:" + str(
+            self.humidity) + "%" + ',' + "TempMin:" + str(self.tempmin) + ',' + "TempMax:" + str(self.tempmax) + ',' + self.descripcion
+        self.send_data(self.data)
+
+    def update_systick(self):
+        # Incrementar el contador systick
+        self.systick += 1
+        print(self.systick)
+        # Perform task 1 every 5 min
+        # if self.systick  % 300 == 0:
+        if self.systick % 10 == 0:
+            # Se obtiene la hora actual
+            self.current_time = QTime.currentTime()
+        # Se verifica si está dentro del intervalo de tiempo deseado
+            if self.current_time >= QTime(9, 0) and self.current_time <= QTime(22, 0):
+                self.shown_on_display_coming_event()
+         # Perform task 1 every 30 minutos
+        if self.systick % 1800 == 0:
+            self.fetch_weather()
+
+    # All related Mqtt functions
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected With Result Code " + str(rc))
@@ -186,45 +282,26 @@ class Window(QWidget):
         self.update_events_listview()
         self.update_calendar_status()
 
-    ################################################################
-    # The callback for when the broker responds to our connection request.
-    # def on_connect(self, client, userdata, flags, rc):
-    #     self.window.write("Connected to server with with flags: %s, result code: %s" % (flags, rc))
+    def find_serial_ports(self):
+        ports = QSerialPortInfo.availablePorts()
+        for port in ports:
+            self.serial_port = SerialPort(port.portName())
+            if self.serial_port.isOpen():
+                print(f"Connected to {port.portName()}")
+                self.serial_port.readyRead.connect(self.receive_data)
+                break
+        else:
+            print("No serial port found")
 
-    #     if rc == 0:
-    #         print("Connection succeeded.")
+    def send_data(self, data):
+        if self.serial_port:
+            self.serial_port.write_data(data)
 
-    #     elif rc > 0:
-    #         if rc < len(mqtt_rc_codes):
-    #             print("Connection failed with error: %s", mqtt_rc_codes[rc])
-    #         else:
-    #             print("Connection failed with unknown error %d", rc)
-
-    #     # Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
-    #     client.subscribe(self.subscription)
-    #     self.window.show_status("Connected.")
-    #     self.window.set_connected_state(True)
-    #     return
-
-    # # The callback for when the broker responds with error messages.
-    # def on_log(client, userdata, level, buf):
-    #     print("on_log level %s: %s", level, userdata)
-    #     return
-
-    # def on_disconnect(self, client, userdata, rc):
-    #     print("disconnected")
-    #     self.window.write("Disconnected from server.")
-    #     self.window.show_status("Disconnected.")
-    #     self.window.set_connected_state(False)
-
-    # # The callback for when a message has been received on a topic to which this
-    # # client is subscribed.  The message variable is a MQTTMessage that describes
-    # # all of the message parameters.
-
-    # # Some useful MQTTMessage fields: topic, payload, qos, retain, mid, properties.
-    # #   The payload is a binary string (bytes).
-    # #   qos is an integer quality of service indicator (0,1, or 2)
-    # #   mid is an integer message ID.
+    def receive_data(self):
+        if self.serial_port:
+            data = self.serial_port.read_data()
+            if data:
+                print(f"Received: {data}")
 
 
 if __name__ == "__main__":
